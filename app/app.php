a67246043456b26c87740e57d9d8ca1b394e359e
Ayana Powell and Katy Henning <ayana@>
2016-09-07 11:10:06 -0700

    html content transferred to twig template

--- app/app.php
+++ app/app.php
@@ -8,58 +8,30 @@
       $_SESSION['list_of_tasks'] = array();
     }
 
-
     $app = new Silex\Application();
+
+//notifying project twig exists
+    $app->register(new Silex\Provider\TwigServiceProvider(), array (
+      'twig.path' => __DIR__.'/../views'
+    ));
+
 //creates home pages
-    $app->get("/" , function() {
-
-      $output = "";
-
-      $all_tasks = Task:: getAll(); //variable equals function in list.php
-
-      if (!empty($all_tasks)) {
-        $output = $output . "
-            <h1>To Do List</h1>
-            <p>Here are all your tasks:</p>
-        ";
-
-        foreach ($all_tasks as $task) {
-            $output .= "<p>" . $task->getDescription() . "</p>";
-        }
-      }
-//form that will take new task and append to /tasks page
-      $output .=  "
-        <form action='/tasks' method='post'>
-          <label for='description'>Task Description</label>
-          <input id='description' name='description' type='text'>
-          <button type='submit'>Add Task</button>
-        </form>
-      ";
-//Button that deletes previously posted tasks
-      $output .= "
-          <form action='/delete_tasks' method='post'>
-            <button type='submit'>delete</button>
-          </form>
-      ";
-      return $output;
+    $app->get("/", function() use ($app) {
+      return $app['twig']->render('tasks.html.twig', array('tasks' =>
+      Task::getAll()));
     });
+
 //Appends user input from form into /tasks page
-    $app->post("/tasks", function(){
+    $app->post("/tasks", function() use ($app) {
       $task = new Task($_POST['description']);
       $task->save();
-      return "
-          <h1>You created a task!</h1>
-          <p>" . $task->getDescription() . "</p>
-          <p><a href='/'>View your list of things to do.</a></p>
-      ";
+      return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
     });
+
 //Sends user to page /delete_tasks which runs function of emptying the tasks array and sends user back to home. REFRESH!
-    $app->post("/delete_tasks", function() {
+    $app->post("/delete_tasks", function() use ($app) {
       Task::deleteAll();
-      return "
-      <h1>List Cleared!</h1>
-      <p><a href='/'>Home</a></p>
-      ";
+      return $app['twig']->render('delete_tasks.html.twig');
     });
     return $app;
 ?>
